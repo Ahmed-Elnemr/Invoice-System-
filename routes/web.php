691bf72aff56a21fa89ee066c3489105aa1e9513
Ahmed-Elnemr <ahmedelnemr064@gmail.com>
2022-12-22 00:36:03 +0200

    add invoice.blade & add index in InvoiceController

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InvoiceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,5 +23,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('invoices', InvoiceController::class);
 
 Route::get('/{page}', [AdminController::class,'index']);
