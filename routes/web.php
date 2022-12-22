3fca05f175191886752c1e440d0e21f44ed7b780
Ahmed-Elnemr <ahmedelnemr064@gmail.com>
2022-12-22 13:43:12 +0200

    add section -mcr & view

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\SectionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,5 +25,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('invoices', InvoiceController::class);
+Route::resource('sections', SectionController::class);
 
 Route::get('/{page}', [AdminController::class,'index']);
